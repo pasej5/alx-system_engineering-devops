889545ab45e4afdcd113624e7b8403c9e7a79e21
Jealous Matsikachando <pasej5@gmail.com>
2023-08-02 00:48:06 -0700

    expression that will match the above cases

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 #regular expression that will match the given cases
-#regex should not contain square brackets
 
 
-puts ARGV[0].scan(/(^hbt*n$)/).join
+puts ARGV[0].scan(/^hbt*n$/).join
