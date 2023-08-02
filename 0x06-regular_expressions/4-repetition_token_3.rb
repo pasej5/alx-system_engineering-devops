5020852944cc898a4344e82b6baf3da164866242
Jealous Matsikachando <pasej5@gmail.com>
2023-08-02 00:17:07 -0700

    match strings in text processing

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+#regular expression that will match the given cases
+# regex should not contain square brackets
 
 
-puts ARGV[0].scan(/(^h.n$)/).join("\n)
+puts ARGV[0].scan(/(^hbt*n$)/).join("\n)
