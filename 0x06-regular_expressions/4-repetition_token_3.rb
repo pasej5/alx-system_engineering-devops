063d6fb8b6f72509c55f1054589911476ff3f5b1
Jealous Matsikachando <pasej5@gmail.com>
2023-08-02 00:19:10 -0700

    4-repetition_token_3.rb

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #regular expression that will match the given cases
-# regex should not contain square brackets
+#regex should not contain square brackets
 
 
-puts ARGV[0].scan(/(^hbt*n$)/).join("\n)
+puts ARGV[0].scan(/(hbt*n)/).join("\n)
