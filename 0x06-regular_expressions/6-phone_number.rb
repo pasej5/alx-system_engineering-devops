da8f6c379b4516b77e6bd4389b1fd420583cc8be
Jealous Matsikachando <pasej5@gmail.com>
2023-08-02 01:51:08 -0700

    The regular expression must match a 10 digit phone numbe

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #The regular expression must match a 10 digit phone numbe
 
-puts ARGV[0].scan(/(^[0-9]{3}-[0-9]{3}-[0-9]{4}$)/).join
+puts ARGV[0].scan(/(^[0-9]{3}-?[0-9]{3}-?[0-9]{4}$)/).join
