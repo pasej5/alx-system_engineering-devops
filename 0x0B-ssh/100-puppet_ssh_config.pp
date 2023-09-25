be9b82bc62d13abcf405c3b6e6a8dbadb4d87c12
Jealous Matsikachando <pasej5@gmail.com>
2023-09-24 19:52:19 -0700

    puppet fie

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-#puppet file
+#using a puppet file
 
-exec { 'append_ssh_config':
-  command => "echo -e '\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no' >> /etc/ssh/ssh_config",
+$content = '\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication\no'
+exec {'ssh_config':
   path    => '/usr/bin',
-  unless  => "grep -q 'IdentityFile ~/.ssh/school' /etc/ssh/ssh_config && grep -q 'PasswordAuthentication no' /etc/ssh/ssh_config",
-  require => File['/etc/ssh/ssh_config'],
+  command => "echo '${content}' >> /etc/ssh/ssh_config"
 }
