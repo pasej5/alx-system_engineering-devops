a24af6940eb2afbf0ec042903fa0c08d7ad15d91
Jealous Matsikachando <pasej5@gmail.com>
2023-10-23 08:01:56 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,7 @@
 import requests
 import sys
 
-
-if __name__ == "__main__":
+if __name__ == "__main":
     employee_id = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com/users/'
     emp_url = url + employee_id
@@ -12,15 +11,15 @@ if __name__ == "__main__":
 
     emp_res = requests.get(emp_url).json()
     todo_res = requests.get(todo_url).json()
-    empl_name = emp_res.get('name')
+    emp_name = emp_res.get('name')
     total = len(todo_res)
     done = 0
     for i in todo_res:
         if i.get("completed"):
             done += 1
     print("Employee {} is done with tasks({}/{}):".format(
-                                    emp_name, done, total))
+        emp_name, done, total))
     for todo in todo_res:
-        if todo.get("complicated"):
-            tittle = todo.get('title')
-            print("\t_{}".format(title))
+        if todo.get("completed"):
+            title = todo.get('title')
+            print("\t{}".format(title))
