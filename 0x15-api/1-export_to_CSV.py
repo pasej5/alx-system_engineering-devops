9c06822eb83376b60fe8bd0e6b16af71dd2dc28d
Jealous Matsikachando <pasej5@gmail.com>
2023-10-23 12:12:07 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees and exporting to CSV"""
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
 import sys
-import csv
+
 
 if __name__ == '__main__':
     employeeId = sys.argv[1]
@@ -11,23 +11,14 @@ if __name__ == '__main__':
     url = baseUrl + "/" + employeeId
 
     response = requests.get(url)
-    employeeName = response.json().get('name')
+    username = response.json().get('username')
 
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
     tasks = response.json()
-    done_tasks = []
 
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-
-    csv_file_name = f"{employeeId}.csv"
-    with open(csv_file_name, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        for task in done_tasks:
-            csv_writer.writerow([employeeId, employeeName, "Completed", task.get('title')])
-
-    print(f"Data exported to {csv_file_name}")
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
